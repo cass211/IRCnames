3e80eb38ea5124c1a7b20c73acb77a30547c6b9a
Cindy Opti <cindy@opti7010>
2022-01-12 11:38:56 -0800

    modified rasPi2.py to run on python3

--- rasPi2.py
+++ rasPi2.py
@@ -1,3 +1,4 @@
+
 # runs with python2
 # From the Raspberry Pi User Guide p188
 
@@ -28,32 +29,34 @@ SOCK_STREAM is the socket type (the default).
 '''
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print 'Connecting to %(host)s:%(port)s...' % irc
+print( 'Connecting to %(host)s:%(port)s...' % irc)
 try:
     s.connect((irc['host'], irc['port']))
 except socket.error as se:
-    print 'Error connecting to IRC server %(host)s:%(port)s' % irc
-    print se
+    print( 'Error connecting to IRC server %(host)s:%(port)s') % irc
+    print( se)
     sys.exit(1)
 
 print("connect succeeded")
 
 #exit()
 
-s.send('NICK %(nick)s\r\n' % user)
+#s.send('NICK %(nick)s\r\n'.encode() % user)
+encodedNick = ('NICK %(nick)s\r\n' % user).encode()
+s.send(encodedNick)
 print('sent NICK %(nick)s\r\n' % user)
-s.send('USER %(username)s %(hostname)s %(servername)s :%(realname)s\r\n' % user)
+s.send(('USER %(username)s %(hostname)s %(servername)s :%(realname)s\r\n' % user).encode())
 print('sent USER %(username)s %(hostname)s %(servername)s :%(realname)s\r\n' % user)
-s.send('JOIN %(channel)s\r\n' % irc)
+s.send(('JOIN %(channel)s\r\n' % irc).encode())
 print('sent JOIN %(channel)s\r\n' % irc)
-s.send('NAMES %(channel)s\r\n' % irc)
+s.send(('NAMES %(channel)s\r\n' % irc).encode())
 print('sent NAMES %(channel)s\r\n' % irc)
 
 read_buffer = ''
 names = []
 
 while True:
-    read_buffer += s.recv(1024)
+    read_buffer += (s.recv(1024).decode())
     print("read next 1024")
     lines = read_buffer.split('\r\n')
     read_buffer = lines.pop();     # that semicolon is in both copies of code
@@ -68,12 +71,12 @@ while True:
             names += names_list.split(' ')
         if response_code == RPL_ENDOFNAMES:
             print('resp. was RPL_ENDOFNAMES')
-            print '\r\nUsers in %(channel)s:' % irc
+            print( '\r\nUsers in %(channel)s:') % irc
         for name in names:
-            print name
+            print( name)
         #exit()
         names = []
         time.sleep(irc['namesinterval'])
-        s.send('NAMES %(channel)s\r\n' % irc)
+        s.send(('NAMES %(channel)s\r\n' % irc).encode())
 
 
