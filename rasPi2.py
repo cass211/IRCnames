333d94262907954ed8c10f73b41a11ecb4e8678a
Jim <jim@jam.sessionsnet.org>
2022-01-11 18:43:15 -0800

    add print statements to show us what's going on

--- rasPi2.py
+++ rasPi2.py
@@ -36,11 +36,16 @@ except socket.error as se:
     print se
     sys.exit(1)
 
+print("connect succeeded")
+
 #exit()
 
 s.send('NICK %(nick)s\r\n' % user)
+print('sent NICK %(nick)s\r\n' % user)
 s.send('USER %(username)s %(hostname)s %(servername)s :%(realname)s\r\n' % user)
+print('sent USER %(username)s %(hostname)s %(servername)s :%(realname)s\r\n' % user)
 s.send('JOIN %(channel)s\r\n' % irc)
+print('sent JOIN %(channel)s\r\n' % irc)
 s.send('NAMES %(channel)s\r\n' % irc)
 
 time.sleep(10)
@@ -51,15 +56,20 @@ names = []
 
 while True:
     read_buffer += recv(1024)
+    print("read next 1024")
     lines = read_buffer.split('\r\n')
     read_buffer = lines.pop();     # that semicolon is in both copies of code
     for line in lines:
+        print('for line in lines')
         response = line.rstrip().split(' ', 3)
         response_code = response[1]
+        print( 'response code is ' + str(response_code) )
         if response_code == RPL_NAMREPLY:
+            print('resp. was RPL_NAMREPLY')
             names_list = response[3].split(':')[1]
             names += names_list.split(' ')
         if response_code == RPL_ENDOFNAMES:
+            print('resp. was RPL_ENDOFNAMES')
             print '\r\nUsers in %(channel)s:' % irc
         for name in names:
             print name
